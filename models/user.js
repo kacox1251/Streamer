4833abe3713d37d59983e5b0447b568ab9229986
ambernina <rob.ambernina@gmail.com>
2020-02-22 09:59:48 -0700

    removed id from model as it is not necessary with sequelize

--- models/user.js
+++ models/user.js
@@ -3,12 +3,6 @@ var bcrypt = require("bcryptjs");
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
 	var User = sequelize.define("User", {
-		id: {
-			type: DataTypes.INTEGER,
-			autoIncrement: true,
-			allowNull: false,
-			primaryKey: true
-		},
 		// The email cannot be null, and must be a proper email before creation
 		email: {
 			type: DataTypes.STRING,
