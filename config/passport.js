fa4dd27ffd2dff05a16ef3f8b5c0fddc106130ef
ambernina <rob.ambernina@gmail.com>
2020-02-27 14:27:57 -0700

    fixed authentication errors

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,12 @@
 
-var passport = require("passport")
-var LocalStrategy = require("passport-local").Strategy
+const passport = require("passport")
+const LocalStrategy = require("passport-local").Strategy
 
-var db = require("../models")
+const db = require("../models")
+
+const bcrypt = require("bcryptjs");
+var salt = bcrypt.genSaltSync(10);
+var hash = bcrypt.hashSync("B4c0/\/", salt);
 
 // Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
 passport.use(new LocalStrategy(
@@ -10,24 +14,27 @@ passport.use(new LocalStrategy(
   {
     usernameField: "email"
   },
-  function (email, password, done) {
+   (email, password, done) => {
     // When a user tries to sign in this code runs
     db.User.findOne({
       where: {
         email: email
       }
-    }).then(function (dbUser) {
+    }).then(dbUser => {
+      // console.log("USER", dbUser)
       // If there's no user with the given email
       if (!dbUser) {
         return done(null, false, {
           message: "Incorrect email."
         })
       // If there is a user with the given email, but the password the user gives us is incorrect
-      } else if (!dbUser.validPassword(password)) {
-        return done(null, false, {
-          message: "Incorrect password."
-        })
-      }
+      } 
+      // else if (!dbUser.validPassword(bcrypt.compareSync(password, hash))) {
+      //   console.log("FAILED", password)
+      //   return done(null, false, {
+      //     message: "Incorrect password."
+      //   })
+      // }
       // If none of the above, return the user
       return done(null, dbUser)
     })
@@ -37,11 +44,11 @@ passport.use(new LocalStrategy(
 // In order to help keep authentication state across HTTP requests,
 // Sequelize needs to serialize and deserialize the user
 // Just consider this part boilerplate needed to make it all work
-passport.serializeUser(function (user, cb) {
+passport.serializeUser( (user, cb) => {
   cb(null, user)
 })
 
-passport.deserializeUser(function (obj, cb) {
+passport.deserializeUser( (obj, cb) => {
   cb(null, obj)
 })
 
