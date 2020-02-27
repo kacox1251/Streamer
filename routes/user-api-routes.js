a790d1471ba0f08abc18a365af60015e257aba55
ambernina <rob.ambernina@gmail.com>
2020-02-27 14:40:56 -0700

    changed the redirect in the signup route to a success message sent to front end to ensure the front end function runs

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,43 +1,44 @@
+const db = require("../models");
+require("dotenv").config();
+const passport = require("../config/passport");
 
-const db = require('../models')
-require('dotenv').config()
-const passport = require('../config/passport')
+module.exports = app => {
+	// app.post for login
+	app.post("/api/login", passport.authenticate("local"), (req, res) => {
+		res.json(req.user)
+	});
 
-module.exports = function (app) {
-  // app.post for login
-  app.post('/api/login', passport.authenticate('local'), function (req, res) {
-    // console.log("req user in user api routes", req.user)
-    res.json(req.user)
-  })
+	// app.post for signup
+	app.post("/api/signup", (req, res) => {
+		db.User.create({
+			username: req.body.username,
+			email: req.body.email,
+			password: req.body.password
+		})
+			.then(() => {
+				res.json({ message: "success" });
+				// res.redirect(307, '/login')
+			})
+			.catch(e => {
+				res.status(401).json(e);
+			});
+	});
 
-  // app.post for signup
-  app.post('/api/signup', function (req, res) {
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password
-    }).then(function () {
-      res.redirect(307, '/api/login')
-    })
-      .catch(function (err) {
-        res.status(401).json(err)
-      })
-  })
+	// app.get for logout
+	app.get("/logout", (req, res) => {
+		req.logout();
+		res.redirect("/");
+	});
 
-  // app.get for logout
-  app.get('/logout', function (req, res) {
-    req.logout()
-    res.redirect('/')
-  })
-
-  // Route for getting some data about our user to be used client side
-  app.get('/api/user_data', function (req, res) {
-    if (!req.user) {
-      res.json({})
-    } else {
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      })
-    }
-  })
-}
+	// Route for getting some data about our user to be used client side
+	app.get("/api/user_data", (req, res) => {
+		if (!req.user) {
+			res.json({});
+		} else {
+			res.json({
+				email: req.user.email,
+				id: req.user.id
+			});
+		}
+	});
+};
