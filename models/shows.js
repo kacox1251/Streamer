c775b1ca2bdd4b9bac8aad8d1ec6d5c473c53b08
ambernina <rob.ambernina@gmail.com>
2020-02-27 14:28:24 -0700

    added poster path and refactored to arrow functions

--- models/shows.js
+++ models/shows.js
@@ -1,21 +1,16 @@
-// id
-// title
-// genre
-// want to watch
-// completed default false
-// watching default false
-// foreign key pointing to user id, pointing to user id will come in with the route files with the where and includes stuff
 
-module.exports = function (sequelize, DataTypes) {
-	var Shows = sequelize.define("Shows", {
+module.exports = (sequelize, DataTypes) => {
+	const Shows = sequelize.define("Shows", {
 		api_id: {
 			type: DataTypes.INTEGER,
 			allowNull: false,
-			// primaryKey: true
 		},
 		title: {
 			type: DataTypes.STRING,
 			allowNull: false
+		},
+		poster_path: {
+			type: DataTypes.STRING
 		},
 		want_to_watch: {
 			type: DataTypes.BOOLEAN
@@ -30,7 +25,7 @@ module.exports = function (sequelize, DataTypes) {
 		freezeTableName: true
 	});
 
-	Shows.associate = function (models) {
+	Shows.associate = models => {
 		Shows.belongsTo(models.User, {
 			foreignKey: {
 				allowNull: false
