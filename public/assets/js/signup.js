beb7a2acf118c6464a525b79425b844545284dc9
ambernina <rob.ambernina@gmail.com>
2020-02-27 14:38:15 -0700

    refactored to arrow functions and changed signup post to send data back to front end so that the function will run there

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,57 +1,68 @@
 // console.log("signup js here");
 
-$(document).ready(function () {
-  console.log("signup doc ready");
+$(document).ready(function() {
+	console.log("signup doc ready");
 
-  // Getting references to our form and input
-  // var signUpForm = $("#signUpForm");
-  var userNameInput = $("#signup-username")
-  var emailInput = $("#signup-email");
-  var passwordInput = $("#signup-pwd");
+	// Getting references to our form and input
+	// var signUpForm = $("#signUpForm");
+	const usernameInput = $("#signup-username");
+	const emailInput = $("#signup-email");
+	const passwordInput = $("#signup-pwd");
 
-  // When the signup button is clicked, we validate the email and password are not blank
-  // $("#signup-btn").click(function (event) {
-  //   event.preventDefault();
-  //   // $("#signUpForm").submit();
-  //   console.log("Did it do it?");
-  // }).then(function () {
-  //   data.redirect("/profile");
-  // })
+	// When the signup button is clicked, we validate the email and password are not blank
+	// $("#signup-btn").click(function (event) {
+	//   event.preventDefault();
+	//   // $("#signUpForm").submit();
+	//   console.log("Did it do it?");
+	// }).then(function () {
+	//   data.redirect("/profile");
+	// })
 
-  $("#signUpForm").on("submit", function (event) {
-    event.preventDefault();
-    console.log("Did it do it?");
-    var userData = {
-      userName: userNameInput.val().trim(),
-      email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
-    };
-    console.log(userData);
-    if (!userData.email || !userData.password) {
-      return;
-    }
-    // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
-    emailInput.val("");
-    passwordInput.val("");
-  });
+	$("#signUpForm").on("submit", event => {
+		event.preventDefault();
+		console.log("Did it do it?");
+		const userData = {
+			username: usernameInput.val().trim(),
+			email: emailInput.val().trim(),
+			password: passwordInput.val().trim()
+		};
+		console.log(userData);
+		if (!userData.email || !userData.password) {
+			return;
+		}
+		// If we have an email and password, run the signUpUser function
+    signUpUser(userData.username, userData.email, userData.password);
+		usernameInput.val("");
+		emailInput.val("");
+		passwordInput.val("");
+	});
 
-  // Does a post to the signup route. If successful, we are redirected to the members page
-  // Otherwise we log any errors
-  function signUpUser(email, password) {
-    $.post("/api/signup", {
-      email: email,
-      password: password
-    })
-      .then(function (data) {
-        window.location.replace("/login");
-        // If there's an error, handle it by throwing up a bootstrap alert
-      })
+	// Does a post to the signup route. If successful, we are redirected to the members page
+	// Otherwise we log any errors
+	function signUpUser(username, email, password) {
+    console.log("sign up user function");
+		$.post("/api/signup", {
+      username: username,
+			email: email,
+			password: password
+		})
+			.then(data => {
+        // res.json(data);
+        console.log("got here" + data);
+        // ifNewUser();
+        if(data.message === "success") {
+          window.location.replace("/login");
+        } else {
+          console.log("fail")
+        }
+				// If there's an error, handle it by throwing up a bootstrap alert
+			})
       .catch(handleLoginErr);
   }
 
-  function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON);
-    $("#alert").fadeIn(500);
-  }
+
+	function handleLoginErr(err) {
+		$("#alert .msg").text(err.responseJSON);
+		$("#alert").fadeIn(500);
+	}
 });
