14ffcfc54979de436fb6e514b9976b168e29a172
ambernina <rob.ambernina@gmail.com>
2020-02-29 02:47:51 -0700

    fleshed out click events for buttons

--- public/assets/js/selected.js
+++ public/assets/js/selected.js
@@ -68,9 +68,6 @@ $(document).ready(function () {
       title: $(".title").text().trim(),
       media_type: $(".posterImage").attr("data-media"),
       poster_path: $(".posterImage").attr("data-poster"),
-      want_to_watch: true,
-      watching: false,
-      completed: false,
       UserId: localStorage.getItem("user-id")
     };
     console.log("add " + JSON.stringify(dataPass))
@@ -79,61 +76,83 @@ $(document).ready(function () {
       title: dataPass.title,
       media_type: dataPass.media_type,
       poster_path: dataPass.poster_path,
-      want_to_watch: dataPass.want_to_watch,
-      watching: dataPass.watching,
-      completed: dataPass.completed,
+      want_to_watch: true,
+      watching: false,
+      completed: false,
       UserId: dataPass.UserId
     }).then(function(data) {
-      alert("we did something", data);
+      console.log("is this thing on?");
+      if(data.message === "success") {
+        alert("Added to your watchlist!");
+      }
+    }).catch(function(e) {
+      console.log(e);
     })
      
   });
 
-  $("#mark-start").click(function (event) {
+  $(".mark-start").click(function (event) {
     event.preventDefault();
-    //define data based on what is on the page
+    console.log("something happened")
+    ///define data based on what is on the page
     let dataPass = {
-      api_id: $(this).attr("#data-movie"),
-      title: $(this).value("#movie-title"),
+      api_id: $(".posterImage").attr("data-id"),
+      title: $(".title").text().trim(),
+      media_type: $(".posterImage").attr("data-media"),
+      poster_path: $(".posterImage").attr("data-poster"),
+      UserId: localStorage.getItem("user-id")
+    };
+    console.log("add " + JSON.stringify(dataPass))
+    $.post("/api/watchlist", {
+      api_id: dataPass.api_id,
+      title: dataPass.title,
+      media_type: dataPass.media_type,
+      poster_path: dataPass.poster_path,
       want_to_watch: false,
       watching: true,
       completed: false,
-      // foreignKey: // req.user.id or $(this) req user id?
-    }
-
-    $.post("/api/selected/:id", // pass data
-      {
-        dataPass
-      },
-      function (data, status) {
-        // console.log("Data: " + data + " || Status: " + status);
-        // then redirect to profile
-        location.href("/profile");
-      });
+      UserId: dataPass.UserId
+    }).then(function(data) {
+      console.log("is this thing on?");
+      if(data.message === "success") {
+        alert("This title is now on your watching list");
+      }
+    }).catch(function(e) {
+      console.log(e);
+    })
+     
   });
 
-  $("#mark-complete").click(function (event) {
+  $(".mark-complete").click(function (event) {
     event.preventDefault();
-    //define data based on what is on the page
+    console.log("something happened")
+    ///define data based on what is on the page
     let dataPass = {
-      api_id: $(this).attr("#data-movie"),
-      title: $(this).value("#movie-title"),
+      api_id: $(".posterImage").attr("data-id"),
+      title: $(".title").text().trim(),
+      media_type: $(".posterImage").attr("data-media"),
+      poster_path: $(".posterImage").attr("data-poster"),
+      UserId: localStorage.getItem("user-id")
+    };
+    console.log("add " + JSON.stringify(dataPass))
+    $.post("/api/watchlist", {
+      api_id: dataPass.api_id,
+      title: dataPass.title,
+      media_type: dataPass.media_type,
+      poster_path: dataPass.poster_path,
       want_to_watch: false,
       watching: false,
       completed: true,
-      // foreignKey: // req.user.id or $(this) req user id?
-    }
-
-    $.post("/api/selected/:id", // pass data
-      {
-        dataPass
-      },
-      function (data, status) {
-        // console.log("Data: " + data + " || Status: " + status);
-
-        // then redirect to profile
-        location.href("/profile");
-      });
+      UserId: dataPass.UserId
+    }).then(function(data) {
+      console.log("is this thing on?");
+      if(data.message === "success") {
+        alert("This title has been completed!");
+      }
+    }).catch(function(e) {
+      console.log(e);
+    })
+     
   });
 
 
